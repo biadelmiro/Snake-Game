f2e7ae19278a107ec231e83641b14d1da33dd097
Souza <beatriz.d.souza@accenture.com>
2020-04-28 20:08:05 -0300

    Adicionando o alert

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ function update(event){
 }
 
 function iniciarJogo(){
+
     //Lógica do plano cateziano, temos o ponto 0 de x e 0 de y , passando disso ele aumenta ou diminui
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0; 
     //se o valor de snake [0 que é sua cabeça] for maior que 15* o tamanho da box e ela estiver indo para a direita, automaticamente ela sai da box
@@ -52,6 +53,13 @@ function iniciarJogo(){
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    for(i=1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     criarCobrinha();
