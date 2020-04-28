cee4c3693e2c3b8d7b002ecfa87971ca19187719
Souza <beatriz.d.souza@accenture.com>
2020-04-28 17:00:18 -0300

    adicionando movimento a cobrinha

--- script.js
+++ script.js
@@ -22,8 +22,30 @@ function criarCobrinha(){
 }
 
 function iniciarJogo(){
-    
+    criarBG();
+    criarCobrinha();
+
+    //criar posição x e y para que a cobrinha saiba qual é o ponto de partida
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    //criar as coordenadas para que ela saiba onde vai, se ela for para o lado 
+    //direito ele adiciona um quadrado no lado direito, se for pro esquerdo, ele diminui um por exemplo
+
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeY -= box; //diminui pq é um plano carteziano então consequentemente ele diminui indo pra esquerda e aumenta indo pra direita
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+
+    snake.pop(); //retira o ultimo elemento do array
+
+    let newHead ={ //sempre vai ter que ter a cabeça
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
 }
 
-criarBG();
-criarCobrinha();
+let jogo = setInterval(iniciarJogo, 100); // Está passando um intervalo de 100 millisegundos para que o jogo não trave
